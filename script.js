3a05a8c71ade0717cf98c7dd56c8d940112082cf
jprigdao0716 <75346728+jprigdao0716@users.noreply.github.com>
2024-05-10 08:58:03 +0800

    script.js

--- script.js
+++ script.js
@@ -19,4 +19,44 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-    
+
+
+// select dom items
+const menuBtn = 
+    document.querySelector(".menu-btn");
+
+const menu = 
+    document.querySelector(".navbar");
+
+const menuNav = 
+    document.querySelector(".navbar-collapse");
+
+const navItems = 
+    document.querySelectorAll(".nav-item");
+
+// Set the initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        navItems.forEach((item) =>
+            item.classList.add("show"));
+
+        // Reset the menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        navItems.forEach((item) =>
+            item.classList.remove("show"));
+
+        // Reset the menu state
+        showMenu = false;
+    }
+}
